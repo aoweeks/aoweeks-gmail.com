598baf87cc079c8963505a9e26c61a35221dedb1
Adam Weeks <aoweeks@gmail.com>
2020-03-26 17:47:01 +0000

    Add title field to Create Post

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -6,9 +6,5 @@ import { Component } from "@angular/core";
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent{
-  posts = [
-    {title: 'First Post', content: 'This is the first post\'s comment'},
-    {title: 'Second Post', content: 'This is the second post\'s comment'},
-    {title: 'Third Post', content: 'This is the third post\'s comment'}
-  ]
+  posts = []
 };
