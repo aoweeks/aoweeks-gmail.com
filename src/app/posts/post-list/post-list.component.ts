fa5b79adaa0c6c6abc36a8ae5cc9d597074bbe50
Adam Weeks <aoweeks@gmail.com>
2020-03-26 16:51:47 +0000

    Add *ngFor to cycle through posts

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -5,4 +5,10 @@ import { Component } from "@angular/core";
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent{};
+export class PostListComponent{
+  posts = [
+    {title: 'First Post', content: 'This is the first post\'s comment'},
+    {title: 'Second Post', content: 'This is the second post\'s comment'},
+    {title: 'Third Post', content: 'This is the third post\'s comment'}
+  ]
+};
