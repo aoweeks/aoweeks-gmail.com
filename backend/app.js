782d7ceb091c34c7f5e91148c728857c42e56059
Adam Weeks <aoweeks@gmail.com>
2020-03-28 17:52:54 +0000

    Add route handling to server

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,39 @@ const express = require('express');
 const app = express();
 
 app.use( (req, res, next) => {
-  console.log('First middleware');
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
   next();
 });
 
+app.post("/api/posts", (req, res, next) => {
 
-app.use( (req, res, next) => {
-  res.send('Hello from express!');
+});
+
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'fasd9713sad1',
+      title: 'First server-side post',
+      content: 'This is coming from the server'
+    },
+    {
+      id: 'k99sjakdh987',
+      title: 'Second server-side post',
+      content: 'This is also coming from the server!'
+    },
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
