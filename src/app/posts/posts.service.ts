431fb232b562c064a78975bbf1c19521f5679bf4
Adam Weeks <aoweeks@gmail.com>
2020-03-30 18:41:25 +0100

    Implement deleting of individual posts

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,6 +2,7 @@ import { Post } from './post.model';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -10,12 +11,22 @@ export class PostsService {
 
   constructor(private http: HttpClient) {}
 
-  //7cDEiKwwxRbyZVrR
+
 
   getPosts(){
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-      .subscribe( (postData) => {
-        this.posts = postData.posts;
+    this.http
+      .get<{message: string, posts: any}>('http://localhost:3000/api/posts')
+      .pipe(map( (postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          }
+        })
+      }))
+      .subscribe( (transformedPosts) => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
@@ -31,6 +42,13 @@ export class PostsService {
         console.log(responseData.message);
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  deletePost(postId: string){
+    this.http.delete('http://localhost:3000/api/posts/' + postId)
+      .subscribe( () => {
+        console.log('Deleted');
       });
   }
 }
