bdb811224f9587fb6d39706c4fb3ad1a6c8b2ef6
Adam Weeks <aoweeks@gmail.com>
2020-03-30 17:59:36 +0100

    Retrieve posts from database

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const Post = require('./models/post');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://adam:7cDEiKwwxRbyZVrR@cluster0-xy4n4.mongodb.net/test?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://adam:7cDEiKwwxRbyZVrR@cluster0-xy4n4.mongodb.net/node-angular?retryWrites=true&w=majority')
   .then( () => {
     console.log('Connected to database!');
   })
@@ -34,29 +34,20 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  console.log(post);
+  post.save();
   res.status(201).json({
     message: "Post added succesfully!"
   });
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'fasd9713sad1',
-      title: 'First server-side post',
-      content: 'This is coming from the server'
-    },
-    {
-      id: 'k99sjakdh987',
-      title: 'Second server-side post',
-      content: 'This is also coming from the server!'
-    },
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
-  });
+  Post.find()
+    .then( documents => {
+      res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: documents
+      });
+    });
 });
 
 module.exports = app;
